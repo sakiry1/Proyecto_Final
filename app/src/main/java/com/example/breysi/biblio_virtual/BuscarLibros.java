b0775ce289c623125d3f2a4eac2209c7ceb03a42
katte <kattnys@gmail.com>
2018-04-29 14:25:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/breysi/biblio_virtual/BuscarLibros.java
@@ -0,0 +1,19 @@
+package com.example.breysi.biblio_virtual;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by breysi on 28/04/2018.
+ */
+
+public class BuscarLibros  extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buscar_anadir_libros);
+    }
+}
